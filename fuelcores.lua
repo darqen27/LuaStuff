aeef8a2b48048eab035b861f9aa7a7e4e5baf1eb
Justin Loehner <darqen27@icloud.com>
2015-12-18 23:34:37 -0800

    Adding breeder core definitions

--- fuelcores.lua
+++ fuelcores.lua
@@ -6,28 +6,45 @@ local computer = require("computer")
 -- local event = require("events") Future use with component.modem()
 
 
-local fuelcore = {}
-
-
 function getAddresslist() -- Too many identical names, must set proxies to be able to interact with all of the Fuel Core's.
-	print("Getting Fuel Core addresses")
-	local compaddresslist = {}
-	fuelcore={}
-	local n=1
-	for k,v in pairs(component.list("FuelCore")) do
-	compaddresslist[n]=k
-	fuelcore[n] = component.proxy(compaddresslist[n])
-	print("Fuel core #" .. n .." address assigned.") 
-	n=n+1
+	local function fissioncores()
+		print("Getting fisson core addresses")
+		local compaddresslist = {}
+		fissioncore={}
+		local n=1
+		for k,v in pairs(component.list("FuelCore")) do
+		compaddresslist[n]=k
+		fissoncore[n] = component.proxy(compaddresslist[n])
+		print("Fission core #" .. n .." address assigned.") 
+		n=n+1
+		end
+	local function breedercores()
+		print("Getting breeder core addresses")
+		local compaddresslist = {}
+		breedercore = {}
+		local n=1
+		for k,v in pairs(component.list("Breeder")) do
+		if k = nil then 
+			return false
+			else
+			compaddresslist[n]=k
+			breedercore[n] = component.proxy(compaddresslist[n])
+			print("Breeder core #" .. n .." address assigned.") 
+			n=n+1
+		end
 	end
+fissioncores()
+breedercores()
 end
+
+
 
 function getTempall()
 	coretemp={}
 	term.clear()
 	local n=1
-	for i=1, #fuelcore do
-	coretemp[n] = fuelcore[n].getTemperature()
+	for i=1, #fissioncore do
+	coretemp[n] = fissioncore[n].getTemperature()
 	print("Core #" .. n .. " temperature is ".. coretemp[n])
 	n=n+1
 	end
@@ -36,9 +53,9 @@ end
 function getTemp(n)
 	coretemp={}
 	term.clear()
-	if not n then return print("Please enter a number between " .. n .. " and " .. #fuelcore) end
-	if n<=0 or n>(#fuelcore) then return print("Please enter a number between " .. n .. " and " .. #fuelcore) end 
-	coretemp[n] = fuelcore[n].getTemperature()
+	if not n then return print("Please enter a number between " .. n .. " and " .. #fissioncore) end
+	if n<=0 or n>(#fissioncore) then return print("Please enter a number between " .. n .. " and " .. #fissioncore) end 
+	coretemp[n] = fissioncore[n].getTemperature()
 	print("Core #" .. n .. " temperature is ".. coretemp[n])	
 end
 
@@ -47,19 +64,19 @@ end
 function checkFuel(n)
 	corefuel={}
 	term.clear()
-	if not n then return print("Please enter a number between 1 and " .. #fuelcore) end
-	if n<=0 or n>(#fuelcore) then return print("Please enter a number between 1 and " .. #fuelcore) end 
-	corefuel[n] = fuelcore[n].checkFuel()
+	if not n then return print("Please enter a number between 1 and " .. #fissioncore) end
+	if n<=0 or n>(#fissioncore) then return print("Please enter a number between 1 and " .. #fissioncore) end 
+	corefuel[n] = fissioncore[n].checkFuel()
 	print("Core #" .. n .. " fuel level is ".. corefuel[n])
 end
 
 	
 function checkFuelall()
-	corefuel={}
+	corefuel = {}
 	term.clear()
 	local n=1
-	for i=1, #fuelcore do
-	corefuel[n] = fuelcore[n].checkFuel()
+	for i=1, #fissioncore do
+	corefuel[n] = fissioncore[n].checkFuel()
 	print("Core #" .. n .. " fuel level is ".. corefuel[n])
 	n=n+1
 	end
@@ -68,9 +85,9 @@ end
 function setCoordsAll()
 	corecoords = {}
 	local n=1
-	for i=1, #fuelcore do
+	for i=1, #fissioncore do
 	corecoords[n]={x=0, y=0, z=0}
-	corecoords[n].x, corecoords[n].y, corecoords[n].z = fuelcore[n].getCoords()
+	corecoords[n].x, corecoords[n].y, corecoords[n].z = fissioncore[n].getCoords()
 	print("Core #" .. n .. " coordinates are ".. corecoords[n].x .. ", " .. corecoords[n].y .. ", " .. corecoords[n].z)
 	n=n+1
 	end
