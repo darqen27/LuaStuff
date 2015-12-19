24a98a90895b8da3cc475e34abc06dab12cb701a
darqen27 <darqen27@icloud.com>
2015-12-19 00:31:25 -0800

    Update fuelcores.lua

--- fuelcores.lua
+++ fuelcores.lua
@@ -7,7 +7,7 @@ local computer = require("computer")
 
 
 function getAddresslist() -- Too many identical names, must set proxies to be able to interact with all of the Fuel Core's.
-	function fissioncores()
+	local function fissioncores()
 		print("Getting fisson core addresses")
 		local compaddresslist = {}
 		fissioncore = {}
@@ -19,7 +19,7 @@ function getAddresslist() -- Too many identical names, must set proxies to be ab
 			n = n+1
 		end
 	end
-	function breedercores()
+	local function breedercores()
 		print("Getting breeder core addresses")
 		local compaddresslist = {}
 		breedercore = {}
