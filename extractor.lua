c2440af351206e22ea29dfaf5840de8b8b978c57
Justin Loehner <darqen27@icloud.com>
2016-01-05 12:21:51 -0800

    Adding new extractor script

---
+++ extractor.lua
@@ -0,0 +1,8 @@
+local component = require("component")
+local redstone = require("redstone")
+local sides = require("sides")
+local rs = component.redstone
+local extractor = component.extractor
+
+
+
