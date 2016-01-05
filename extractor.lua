aaf741c8e6de1381df022e8ffc6dcb2aec75ba71
Justin Loehner <darqen27@icloud.com>
2016-01-05 14:30:50 -0800

    additions

--- extractor.lua
+++ extractor.lua
@@ -8,10 +8,13 @@ function gearSwitch()
 	local checkitems0,checkitems1,checkitems2,checkitems5,checkitems6=extractor.getSlot(0),extractor.getSlot(1),extractor.getSlot(2),extractor.getSlot(5),extractor.getSlot(6)
 	if checkitems1 and checkitems2 == nil
 		then rs.setOutput(sides.south,0)
+		print("Switching to 2x")
 	elseif checkitems5 and checkitems6 == nil
 		then rs.setOutput(sides.south,15)
+		print("Switching to 32x")
 	elseif checkitems0 and checkitems1 and checkitems2 and checkitems5 and checkitems6 == nil
 		then running = false
+		print("fin")
 	end
 end
 rs.setOutput(sides.west,15)
