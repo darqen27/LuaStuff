a74b1aadb36ef4db6eb1c43e8de6cc5baf26c2d5
Justin Loehner <darqen27@icloud.com>
2016-01-06 09:22:34 -0800

    Forming less sloppy tables

--- extractor.lua
+++ extractor.lua
@@ -5,12 +5,12 @@ local extractor = component.Extractor
 local cvt1 = component.proxy("cadee4ce-b9af-45ca-9869-946d1aaf3bc9")
 
 function gearSwitch()
-	local checkitems0,checkitems1,checkitems2,checkitems3,checkitems4,checkitems5,checkitems6=extractor.getSlot(0),extractor.getSlot(1),extractor.getSlot(2),extractor.getSlot(3),extractor.getSlot(4),extractor.getSlot(5),extractor.getSlot(6)
-	if (checkitems3 == nil and checkitems6 == nil) and checkitems4 ~= nil
+	local checkitems0,checkitems1,checkitems2,checkitems3,checkitems4,checkitems5,checkitems6=table.pack(extractor.getSlot(0)),table.pack(extractor.getSlot(1)),table.pack(extractor.getSlot(2)),table.pack(extractor.getSlot(3)),table.pack(extractor.getSlot(4)),table.pack(extractor.getSlot(5)),table.pack(extractor.getSlot(6))
+	if (checkitems3[3]<62 and checkitems6[3] <32) and checkitems4[3]>0
 		then 	
 			rs.setOutput(sides.south,0)
 			print("Switching to 32x")
-	elseif checkitems6 ~= nil
+	elseif (checkitems6 ~=nil or checkitems3[3]>50) 
 		then 
 			rs.setOutput(sides.south,15)
 			print("Switching to 2x")
@@ -22,10 +22,14 @@ function gearSwitch()
 		then
 			rs.setOutput(sides.south,15)
 			print("Starter Cycle")
+	elseif ((checkitems0 ~= nil and checkitems3 ~= nil) and (checkitems1 == nil and checkitems2 == nil)) or ((checkitems6 ~=nil or checkitems3[3]>1) and (checkitems0==nil and checkitems1==nil and checkitems2==nil))
+		then
+			rs.setOutput(sides.south,15)
+			print("Cleaning up")
 	elseif (checkitems0 == nil and checkitems1 == nil and checkitems2 == nil and checkitems3 == nil)
 		then 
 			running = false
-			print("fin")
+			print("Turbine Spinning down!")
 	end
 end
 rs.setOutput(sides.west,15)
@@ -33,7 +37,7 @@ rs.setOutput(sides.north,0)
 running = true
 while running do
 	gearSwitch()
-	os.sleep(1)
+	os.sleep(1.5)
 end
 rs.setOutput(sides.west,0)
 rs.setOutput(sides.north,15)
