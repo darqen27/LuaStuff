9431ea48e56af7a5479a06d791ca8d0dbccce103
darqen27 <darqen27@icloud.com>
2018-10-01 08:29:34 -0600

    typo

--- beerobot.lua
+++ beerobot.lua
@@ -10,10 +10,10 @@ local sides = require("sides")
 function check_network()
 	local event = require("event")
     localip = wan.address
-	setStrength(10)
-	setWakeMessage(Are you there?)
+	wan.setStrength(10)
+	wan.setWakeMessage("Are you there?")
 	print("Opening port " .. tostring(wan.open(001)))
-	print("Connecting = " tostring(wan.broadcast(001, "Are you there?"))
+	print("Connecting = " tostring(wan.broadcast(001, "Are you there?")))
 	local _,_, from, port, _, message = event.pull("modem_message")
 	print("Connected to " .. from .. " with test message: " .. tostring(message))
 	
