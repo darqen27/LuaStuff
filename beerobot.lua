6a36454d7d21c2d272d264cce6b0982f231689bb
Justin L <darqen27@icloud.com>
2018-10-01 01:53:38 -0600

    Update beerobot.lua

--- beerobot.lua
+++ beerobot.lua
@@ -1,1 +1,14 @@
+local component = require("component")
+local robot_api = require("robot")
+local inet = component.internet
+local i_ctrl = component.inventory_controller
+local wan = component.modem
+local sides = require("sides")
+
+
+
+function check_network()
+    wan.open
+
+
 
